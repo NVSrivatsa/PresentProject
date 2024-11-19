b0eaf3e823ed1bb6d08bc3c4405bccc8688b1b68
Srivatsa <srivatsanandanavanamedu@gmail.com>
2024-11-19 14:44:44 +0530

    Demo for Team

---
+++ src/test/java/SEproject/MavenJavaProject/AppTest.java
@@ -0,0 +1,20 @@
+package SEproject.MavenJavaProject;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
